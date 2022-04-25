82e34c2e162ae844b42f68c9cd2926ff1443ece8
shankar87dayal <shankar.87.dayal@gmail.com>
2022-04-25 15:16:20 +0530

    One Good  Practice that makes your project best make one class name AppConstants and put all constant and use  all projects no-27

--- src/main/java/com/codewithraushanblog/controllers/PostController.java
+++ src/main/java/com/codewithraushanblog/controllers/PostController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codewithraushanblog.config.AppConstants;
 import com.codewithraushanblog.entities.Post;
 import com.codewithraushanblog.payloads.ApiResponse;
 import com.codewithraushanblog.payloads.PostDto;
@@ -66,10 +67,10 @@ public class PostController {
 	
 	@GetMapping("/posts")
 	public ResponseEntity<PostResponse> getAllPost(
-			@RequestParam(value = "pageNumber", defaultValue = "0", required = false) Integer pageNumber,       
-			@RequestParam(value = "pageSize", defaultValue = "10", required = false) Integer pageSize,
-			@RequestParam(value = "sortBy", defaultValue = "postId",required = false) String sortBy,
-			@RequestParam(value = "sortDir", defaultValue = "asc",required = false) String sortDir
+			@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,       
+			@RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+			@RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY,required = false) String sortBy,
+			@RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR,required = false) String sortDir
 			
 			)
 	{
