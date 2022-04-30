37ac9b786d8d3009b1cdde19db4c8dfd0adab2e3
shankar87dayal <shankar.87.dayal@gmail.com>
2022-04-30 10:50:55 +0530

    image upload on post Api Working on Post Image in one video Uploading Image for Post Serving Post Image no-28

--- src/main/java/com/codewithraushanblog/controllers/PostController.java
+++ src/main/java/com/codewithraushanblog/controllers/PostController.java
@@ -1,9 +1,17 @@
 package com.codewithraushanblog.controllers;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.engine.jdbc.StreamUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +22,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.codewithraushanblog.config.AppConstants;
 import com.codewithraushanblog.entities.Post;
 import com.codewithraushanblog.payloads.ApiResponse;
 import com.codewithraushanblog.payloads.PostDto;
 import com.codewithraushanblog.payloads.PostResponse;
+import com.codewithraushanblog.services.FileService;
 import com.codewithraushanblog.services.PostService;
 
 @RestController
@@ -28,6 +38,13 @@ public class PostController {
 	
 	@Autowired
 	private PostService postService;
+	
+	
+	@Autowired
+	private FileService fileService;
+	
+	@Value("${project.image}")
+	private String path;
 
 //	create
 	@PostMapping("/user/{idValue}/category/{categoryId}/posts")
@@ -114,6 +131,37 @@ public class PostController {
 		List<PostDto> result = this.postService.searchPosts(keywords);
 		return new ResponseEntity<List<PostDto>>(result, HttpStatus.OK);
 	}
+	
+	@PostMapping("/post/image/upload/{postId}")
+	public ResponseEntity<PostDto> uploadPostImage(
+			@RequestParam("image")MultipartFile image,
+			@PathVariable Integer postId
+			) throws IOException
+	{
+		PostDto postDto = this.postService.getPostById(postId);
+		
+		String fileName = this.fileService.uploadImage(path, image);
+		
+		postDto.setImageName(fileName);
+		PostDto updatePost = this.postService.updatePost(postDto, postId);
+		return new ResponseEntity<PostDto>(updatePost, HttpStatus.OK);
+		
+	}
+	
+	// methods to serve files
+		@GetMapping( value ="/post/image/{imageName}",produces = MediaType.IMAGE_JPEG_VALUE)
+		public void downloadImage(
+				@PathVariable("imageName") String imageName,
+				HttpServletResponse response
+				) throws IOException 
+		{
+		 
+			InputStream resource =this.fileService.getResource(path, imageName);
+			response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+			StreamUtils.copy(resource, response.getOutputStream());
+			
+			
+		}
 	
 }
 
